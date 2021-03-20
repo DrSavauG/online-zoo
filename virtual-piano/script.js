b1415f3f702e23564374efe351da7a52a6cc396e
Andrei Savenok <dr.savaug@gmail.com>
2021-03-20 12:36:20 +0300

    feat: add keyDown, mouseDown

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,1 +1,58 @@
-let a;
+const notes = {
+  KeyD: 'c',
+  KeyF: 'd',
+  KeyG: "e",
+  KeyH: "f",
+  KeyJ: "g",
+  KeyK: "a",
+  'KeyL': "b",
+  KeyR: "c♯",
+  KeyT: "d♯",
+  KeyU: "f♯",
+  KeyI: "g♯",
+  KeyO: "a♯",
+}
+const piano = document.querySelector('.piano');
+
+// console.log(`piano =`, piano);
+// piano.addEventListener('click',(event)=>{
+//     if (event.target.classList.contains('piano-key')){
+//         playAudio('./assets/audio/a.mp3')
+//     }
+// });
+function playAudio(src) {
+  const audio = new Audio();
+  audio.src = src;
+  audio.currentTime = 0;
+  audio.play();
+}
+
+piano.addEventListener('mousedown', (event) => {
+  if (event.target.classList.contains('piano-key')) {
+    const note = event.target.dataset.note;
+    const src = `assets/audio/${note}.mp3`;
+    playAudio(src);
+  }
+});
+
+// || pfзапуск с клавиатуры
+
+function playSoundKey(e) {
+  if (notes[event.code] !== undefined) {
+    const key = document.querySelector(`.piano-key[data-letter="${event.code.slice(-1)}"]`);
+    const audio = new Audio();
+    if (!audio) return;
+    audio.src = `./assets/audio/${notes[event.code]}.mp3`
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('piano-key-active');
+    
+    function removeTransition(e) {
+      // if (e.propertyName !== 'transform') return; //временно убрал
+      this.classList.remove('piano-key-active');
+    }
+    const keys = document.querySelectorAll('.piano-key');
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  }
+}
+window.addEventListener('keydown', playSoundKey);
