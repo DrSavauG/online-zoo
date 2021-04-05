96b6b537685d8e64e740c0578d3bc3037ac2b7ac
Andrei Savenok <dr.savaug@gmail.com>
2021-04-05 19:51:50 +0300

    feat: add toolbar & tooltip

---
+++ online-zoo/pages/zoos/alligator/script.js
@@ -0,0 +1,5 @@
+// const videoBig = document.querySelector('.main__video-big');
+// console.log(`videoBig =`, videoBig.src);
+// videoBig.src = "https://www.youtube.com/watch?v=msPEK0yQyE0"
+
+// // 
