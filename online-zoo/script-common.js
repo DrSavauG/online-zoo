b853ab98e307d90db7d580b328593b724a2b41ad
Andrei Savenok <dr.savaug@gmail.com>
2021-04-18 22:27:58 +0300

    feat: add panda 1200

--- online-zoo/script-common.js
+++ online-zoo/script-common.js
@@ -1,46 +1,58 @@
 const arrow = document.querySelectorAll(".arrow");
-const  text = document.querySelectorAll(".text");
+const text = document.querySelectorAll(".text");
 const block = document.querySelectorAll(".block");
-console.log(arrow);
 
 function addClickEvent(text, block, arrow) {
     arrow.addEventListener('click', () => {
         if (arrow.classList.contains("main-block__arrow-up")) {
             if (block.classList.contains("main-block__animal-down")) {
                 text.style.display = "none";
-                block.classList.remove("main-block__animal-down")
-                block.classList.add("main-block__down-rolled")
-                arrow.classList.remove("main-block__arrow-up", "main-block__arrow")
-                arrow.classList.add("main-block__arrow-rolled", "main-block__arrow-down")
+                block.classList.remove("main-block__animal-down");
+                block.classList.add("main-block__down-rolled");
+                arrow.classList.remove("main-block__arrow-up", "main-block__arrow");
+                arrow.classList.add("main-block__arrow-rolled", "main-block__arrow-down");
             } else {
                 text.style.display = "none";
-                block.classList.remove("main-block__animal")
-                block.classList.add("main-block__rolled")
-                arrow.classList.remove("main-block__arrow-up", "main-block__arrow")
-                arrow.classList.add("main-block__arrow-rolled", "main-block__arrow-down")
+                block.classList.remove("main-block__animal");
+                block.classList.add("main-block__rolled");
+                arrow.classList.remove("main-block__arrow-up", "main-block__arrow");
+                arrow.classList.add("main-block__arrow-rolled", "main-block__arrow-down");
             }
         } else {
             if (block.classList.contains("main-block__down-rolled")) {
                 text.style.display = "block";
-                block.classList.add("main-block__animal-down")
-                block.classList.remove("main-block__down-rolled")
-                arrow.classList.add("main-block__arrow-up", "main-block__arrow")
-                arrow.classList.remove("main-block__arrow-rolled", "main-block__arrow-down")
+                block.classList.add("main-block__animal-down");
+                block.classList.remove("main-block__down-rolled");
+                arrow.classList.add("main-block__arrow-up", "main-block__arrow");
+                arrow.classList.remove("main-block__arrow-rolled", "main-block__arrow-down");
             } else {
                 text.style.display = "block";
-                block.classList.add("main-block__animal")
-                block.classList.remove("main-block__rolled")
-                arrow.classList.add("main-block__arrow-up", "main-block__arrow")
-                arrow.classList.remove("main-block__arrow-rolled", "main-block__arrow-down")
+                block.classList.add("main-block__animal");
+                block.classList.remove("main-block__rolled");
+                arrow.classList.add("main-block__arrow-up", "main-block__arrow");
+                arrow.classList.remove("main-block__arrow-rolled", "main-block__arrow-down");
             }
         }
-    })
+    });
 }
 
 for (let i = 0; i < arrow.length; i++) {
     addClickEvent(text[i], block[i], arrow[i]);
 }
+////////////////////////////////////////////////////////////////
+const toolbarArrow = document.querySelector(".toolbar__arrow");
+const toolbar = document.querySelector(".toolbar");
 
+toolbarArrow.addEventListener("click", () => {
+    if (toolbarArrow.classList.contains("toolbar__arrow--clicked")) {
+        toolbarArrow.classList.remove("toolbar__arrow--clicked");
+        toolbar.classList.remove("toolbar--clicked");
+
+    } else {
+        toolbarArrow.classList.add("toolbar__arrow--clicked");
+        toolbar.classList.add("toolbar--clicked");
+    }
+});
 
 ////////////////////////////
 
@@ -48,12 +60,12 @@ const links = document.querySelector('.youtube');
 let xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://drsavaug.github.io/links/src.json');
 xhr.send();
-xhr.onload = function() {
-  if (xhr.status != 200) {
-    return;
-  }
+xhr.onload = function () {
+    if (xhr.status != 200) {
+        return;
+    }
 };
-xhr.onprogress = function(event) {
-  links.href = JSON.parse(event.target.responseText)['online-zoo'];
-  console.log(links.href);
+xhr.onprogress = function (event) {
+    links.href = JSON.parse(event.target.responseText)['online-zoo'];
+    console.log(links.href);
 };
